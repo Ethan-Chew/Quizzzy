79c1e29c2345025fca3a61609dd3146b18292ebc
Ethan-Chew <ethanchewmh@gmail.com>
2024-05-08 17:41:04 +0800

    wip: Create a new Flashlet

--- app/src/main/java/sg/edu/np/mad/quizzzy/Flashlets/CreateFlashlet.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/Flashlets/CreateFlashlet.java
@@ -1,6 +1,14 @@
 package sg.edu.np.mad.quizzzy.Flashlets;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +16,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sg.edu.np.mad.quizzzy.Models.Flashcard;
+import sg.edu.np.mad.quizzzy.Models.Flashlet;
 import sg.edu.np.mad.quizzzy.R;
 
 public class CreateFlashlet extends AppCompatActivity {
+
+    // Data Variables
+    Flashlet newFlashlet;
+    List<Flashcard> flashcards = new ArrayList<Flashcard>();
+
+    // View Variables
+    Button addNewFlashcardBtn;
+    Button createFlashletBtn;
+    LinearLayout flashcardListView;
+    View newFlashcardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +44,58 @@ public class CreateFlashlet extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Listen for onClick on 'Create Flashlet' button, then create the flashlet
+        addNewFlashcardBtn = findViewById(R.id.cFAddNewFlashcardBtn);
+        createFlashletBtn = findViewById(R.id.cFCreateNewFlashletButton);
+        flashcardListView = findViewById(R.id.cFCreateNewFlashcardList);
+        addNewFlashcardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create new Flashcard object, and add to ArrayList
+                Flashcard newFlashcard = new Flashcard("", "");
+
+                // Inflate the Item for a new Flashcard
+                newFlashcardView = LayoutInflater.from(CreateFlashlet.this).inflate(R.layout.create_flashlet_newflashcard, null, false);
+
+                // Listen for updates in the Flashcard Info
+                EditText keywordEditText = newFlashcardView.findViewById(R.id.newFlashcardKeywordInput);
+                EditText definitionEditText = newFlashcardView.findViewById(R.id.newFlashcardDefinitionInput);
+
+                keywordEditText.addTextChangedListener(new TextWatcher() {
+                    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+                    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        newFlashcard.setKeyword(keywordEditText.getText().toString());
+                    }
+                });
+                definitionEditText.addTextChangedListener(new TextWatcher() {
+                    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+                    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        newFlashcard.setDefinition(definitionEditText.getText().toString());
+                    }
+                });
+
+                // Add Flashcard to List
+                flashcards.add(newFlashcard);
+
+                // Add Inflated View to LinearLayout Container
+                flashcardListView.addView(newFlashcardView);
+
+                // Add Spacer View
+                View spacerView = new View(CreateFlashlet.this);
+                LinearLayout.LayoutParams spacerParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        20
+                );
+                flashcardListView.addView(spacerView, spacerParams);
+            }
         });
     }
 }
--- app/src/main/java/sg/edu/np/mad/quizzzy/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/MainActivity.java
@@ -1,12 +1,17 @@
 package sg.edu.np.mad.quizzzy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import sg.edu.np.mad.quizzzy.Flashlets.CreateFlashlet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button debug = findViewById(R.id.button2);
+        debug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateFlashlet.class);
+                startActivity(intent);
+            }
         });
     }
 }
