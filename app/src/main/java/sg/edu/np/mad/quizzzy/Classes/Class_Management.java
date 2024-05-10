4c88ec7524780df9bb866dd15b57961404ce62b1
mellamadrama <s10258027@connect.np.edu.sg>
2024-05-10 09:48:21 +0800

    class management classes page

---
+++ app/src/main/java/sg/edu/np/mad/quizzzy/Classes/Class_Management.java
@@ -0,0 +1,27 @@
+package sg.edu.np.mad.quizzzy.Classes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import sg.edu.np.mad.quizzzy.R;
+
+public class Class_Management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.class_management);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
