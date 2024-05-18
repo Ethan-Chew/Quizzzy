dbfd5630aae255f934d6def5cbc2e40e14eac718
mellamadrama <s10258027@connect.np.edu.sg>
2024-05-19 01:14:29 +0800

    added classes xml file.

--- app/src/main/java/sg/edu/np/mad/quizzzy/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/MainActivity.java
@@ -1,12 +1,17 @@
 package sg.edu.np.mad.quizzzy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import sg.edu.np.mad.quizzzy.Classes.Add_Class;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Add_Class.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
