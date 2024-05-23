c83fd366867fa4370f9e58c0d4e42a6fae7a019b
Hervin Sie <hervsie@gmail.com>
2024-05-23 21:45:34 +0800

    Added bottom nav bar

--- app/src/main/java/sg/edu/np/mad/quizzzy/HomeActivity.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/HomeActivity.java
@@ -1,22 +1,38 @@
 package sg.edu.np.mad.quizzzy;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import sg.edu.np.mad.quizzzy.Fragments.CreateFragment;
+import sg.edu.np.mad.quizzzy.Fragments.FlashletsFragment;
+import sg.edu.np.mad.quizzzy.Fragments.HomeFragment;
+import sg.edu.np.mad.quizzzy.Fragments.StatsFragment;
 import sg.edu.np.mad.quizzzy.Models.SQLiteManager;
 import sg.edu.np.mad.quizzzy.Models.User;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity  {
 
+    BottomNavigationView bottomNavigationView;
     TextView idView;
     TextView usernameView;
     TextView emailView;
+    HomeFragment homeFragment = new HomeFragment();
+    CreateFragment createFragment = new CreateFragment();
+    FlashletsFragment flashletsFragment = new FlashletsFragment();
+    StatsFragment statsFragment = new StatsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +45,49 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });
 
-        SQLiteManager localDB = SQLiteManager.instanceOfDatabase(HomeActivity.this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
 
-        // Get Screen Elements
-        idView = findViewById(R.id.homeIdView);
-        usernameView = findViewById(R.id.homeUsernameView);
-        emailView = findViewById(R.id.homeEmailView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        SQLiteManager localDB = SQLiteManager.instanceOfDatabase(HomeActivity.this);
 
         // Get User from Database
         User user = localDB.getUser();
 
-        // Set User Info
-        idView.setText(user.getId());
-        usernameView.setText(user.getUsername());
-        emailView.setText(user.getEmail());
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.home) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.flFragment, homeFragment)
+                            .commit();
+                    return true;
+                } else if (itemId == R.id.create) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.flFragment, createFragment)
+                            .commit();
+                    return true;
+                } else if (itemId == R.id.flashlets) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.flFragment, flashletsFragment)
+                            .commit();
+                    return true;
+                } else if (itemId == R.id.stats) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.flFragment, statsFragment)
+                            .commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
+
 }
