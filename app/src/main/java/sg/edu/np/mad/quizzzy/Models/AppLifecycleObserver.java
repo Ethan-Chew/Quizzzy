ac3df18e168cfe0e3bb29b455dc9d36f8d993149
dariusKoh <d4riuskoh@gmail.com>
2024-07-21 17:49:32 +0800

    Allowed checking if the screen is on or off

--- app/src/main/java/sg/edu/np/mad/quizzzy/Models/AppLifecycleObserver.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/Models/AppLifecycleObserver.java
@@ -1,22 +1,41 @@
 package sg.edu.np.mad.quizzzy.Models;
 
+import static android.content.Context.POWER_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Context;
+import android.os.PowerManager;
 import android.util.Log;
 
 public class AppLifecycleObserver implements DefaultLifecycleObserver {
+    public static Context context;
 
+    // This method is called when the app comes into the foreground
     @Override
-    public void onStart(@NonNull LifecycleOwner owner) {
-        // This method is called when the app comes into the foreground
+    public void onStart(LifecycleOwner owner) {
+        boolean screenOn = isScreenOn(context);
+
         Log.d("AppLifecycleObserver", "App moved to foreground");
+        Log.d("AppLifecycleObserver", "Screen is on: " + screenOn);
     }
 
+    // This method is called when the app goes into the background
     @Override
-    public void onStop(@NonNull LifecycleOwner owner) {
-        // This method is called when the app goes into the background
+    public void onStop(LifecycleOwner owner) {
+        boolean screenOn = isScreenOn(context);
+
         Log.d("AppLifecycleObserver", "App moved to background");
+        Log.d("AppLifecycleObserver", "Screen is on: " + screenOn);
+    }
+
+    // Uses power manager to check if the screen is on, and return a boolean value
+    public boolean isScreenOn(Context context) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        return pm.isInteractive();
     }
 }
 
--- app/src/main/java/sg/edu/np/mad/quizzzy/Models/QuizzzyApplication.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/Models/QuizzzyApplication.java
@@ -1,15 +1,21 @@
 package sg.edu.np.mad.quizzzy.Models;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.PowerManager;
+import android.util.Log;
 
 import androidx.lifecycle.ProcessLifecycleOwner;
 
 public class QuizzzyApplication extends Application {
+
+    private PowerManager pm;
     @Override
     public void onCreate() {
         super.onCreate();
 
         AppLifecycleObserver appLifecycleObserver = new AppLifecycleObserver();
+        AppLifecycleObserver.context = getApplicationContext();
         ProcessLifecycleOwner.get().getLifecycle().addObserver(appLifecycleObserver);
     }
 }
