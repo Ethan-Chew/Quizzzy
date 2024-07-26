2a5e2a127ae52a27848d574769180d837411e6b2
mellamadrama <s10258027@connect.np.edu.sg>
2024-07-27 03:10:50 +0800

    fix: scan to join flashlets saving flashletId

--- app/src/main/java/sg/edu/np/mad/quizzzy/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/quizzzy/LoginActivity.java
@@ -107,65 +107,61 @@ public class LoginActivity extends AppCompatActivity {
                                                 if (task.isSuccessful()) {
                                                     DocumentSnapshot document = task.getResult();
 
-                                                        if (document.getData().get("2faSecret") != null) {
-                                                            String secret = document.getData().get("2faSecret").toString();
-                                                            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-                                                            View popupView = inflater.inflate(R.layout.login_2fa_popup, null);
-                                                            PopupWindow popupWindow = getPopupWindow(popupView);
+                                                    if (document.getData().get("2faSecret") != null) {
+                                                        String secret = document.getData().get("2faSecret").toString();
+                                                        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                                                        View popupView = inflater.inflate(R.layout.login_2fa_popup, null);
+                                                        PopupWindow popupWindow = getPopupWindow(popupView);
 
-                                                            // Show the popup window at the center of the layout
-                                                            popupWindow.showAtLocation(v, android.view.Gravity.CENTER, 0, 0);
+                                                        // Show the popup window at the center of the layout
+                                                        popupWindow.showAtLocation(v, android.view.Gravity.CENTER, 0, 0);
 
-                                                            // Dim the background
-                                                            View container = popupWindow.getContentView().getRootView();
-                                                            if (container != null) {
-                                                                WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-                                                                WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
-                                                                p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-                                                                p.dimAmount = 0.5f;
-                                                                if (wm != null) {
-                                                                    wm.updateViewLayout(container, p);
-                                                                }
-                                                            }
-
-                                                            EditText pin1 = popupView.findViewById(R.id.loginPin1);
-                                                            EditText pin2 = popupView.findViewById(R.id.loginPin2);
-                                                            EditText pin3 = popupView.findViewById(R.id.loginPin3);
-                                                            EditText pin4 = popupView.findViewById(R.id.loginPin4);
-                                                            EditText pin5 = popupView.findViewById(R.id.loginPin5);
-                                                            EditText pin6 = popupView.findViewById(R.id.loginPin6);
-
-                                                            pin1.addTextChangedListener(new LoginActivity.TOTPWatcher(pin1, pin2, popupView, secret));
-                                                            pin2.addTextChangedListener(new LoginActivity.TOTPWatcher(pin2, pin3, popupView, secret));
-                                                            pin3.addTextChangedListener(new LoginActivity.TOTPWatcher(pin3, pin4, popupView, secret));
-                                                            pin4.addTextChangedListener(new LoginActivity.TOTPWatcher(pin4, pin5, popupView, secret));
-                                                            pin5.addTextChangedListener(new LoginActivity.TOTPWatcher(pin5, pin6, popupView, secret));
-                                                            pin6.addTextChangedListener(new LoginActivity.TOTPWatcher(pin6, null, popupView, secret));
-
-                                                        } else {
-                                                            if (flashletId != null) {
-                                                                handleFlashletAddition(flashletId, currentUser.getUid());
-                                                                finish();
-                                                            }
-                                                            else {
-                                                                // Save the User to our SQLite (Local) Database
-                                                                SQLiteManager localDB = SQLiteManager.instanceOfDatabase(LoginActivity.this);
-                                                                String userJson = gson.toJson(document.getData());
-                                                                User user = gson.fromJson(userJson, User.class);
-                                                                localDB.addUser(new UserWithRecents(user));
-                                                                // Send User to Home Screen
-                                                                Intent homeScreenIntent = new Intent(LoginActivity.this, HomeActivity.class);
-                                                                startActivity(homeScreenIntent);
+                                                        // Dim the background
+                                                        View container = popupWindow.getContentView().getRootView();
+                                                        if (container != null) {
+                                                            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+                                                            WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+                                                            p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                                                            p.dimAmount = 0.5f;
+                                                            if (wm != null) {
+                                                                wm.updateViewLayout(container, p);
                                                             }
                                                         }
 
-                                                } else {
-                                                    Log.d(TAG, "Get user failed with ", task.getException());
+                                                        EditText pin1 = popupView.findViewById(R.id.loginPin1);
+                                                        EditText pin2 = popupView.findViewById(R.id.loginPin2);
+                                                        EditText pin3 = popupView.findViewById(R.id.loginPin3);
+                                                        EditText pin4 = popupView.findViewById(R.id.loginPin4);
+                                                        EditText pin5 = popupView.findViewById(R.id.loginPin5);
+                                                        EditText pin6 = popupView.findViewById(R.id.loginPin6);
+
+                                                        pin1.addTextChangedListener(new LoginActivity.TOTPWatcher(pin1, pin2, popupView, secret));
+                                                        pin2.addTextChangedListener(new LoginActivity.TOTPWatcher(pin2, pin3, popupView, secret));
+                                                        pin3.addTextChangedListener(new LoginActivity.TOTPWatcher(pin3, pin4, popupView, secret));
+                                                        pin4.addTextChangedListener(new LoginActivity.TOTPWatcher(pin4, pin5, popupView, secret));
+                                                        pin5.addTextChangedListener(new LoginActivity.TOTPWatcher(pin5, pin6, popupView, secret));
+                                                        pin6.addTextChangedListener(new LoginActivity.TOTPWatcher(pin6, null, popupView, secret));
+
+                                                    } else {
+                                                            // Save the User to our SQLite (Local) Database
+                                                            SQLiteManager localDB = SQLiteManager.instanceOfDatabase(LoginActivity.this);
+                                                            String userJson = gson.toJson(document.getData());
+                                                            User user = gson.fromJson(userJson, User.class);
+                                                            localDB.addUser(new UserWithRecents(user));
+                                                        if (flashletId != null) {
+                                                            handleFlashletAddition(flashletId, currentUser.getUid());
+                                                            flashletId = null;
+                                                        } else {
+                                                            // Send User to Home Screen
+                                                            Intent homeScreenIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                                                            startActivity(homeScreenIntent);
+                                                        }
+                                                        }
+
                                                 }
                                             }
                                         });
                                     } else {
-                                        Log.d(TAG, "Login failed.", task.getException());
                                         Toast.makeText(LoginActivity.this, "Login failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                                     }
                                 }
@@ -186,7 +182,6 @@ public class LoginActivity extends AppCompatActivity {
                 if (document.exists()) {
                     List<String> createdFlashlets = (List<String>) document.get("createdFlashlets");
                     if (createdFlashlets != null && createdFlashlets.contains(flashletId)) {
-                        Log.d("LoginActivity", "Flashlet already exists in user createdFlashlets.");
                         navigateToHome(flashletId);
                         Toast.makeText(LoginActivity.this, "You already have this flashlet.", Toast.LENGTH_SHORT).show();
                     } else {
@@ -196,6 +191,7 @@ public class LoginActivity extends AppCompatActivity {
                         localDB.updateCreatedFlashcards(userId, createdFlashletIds);
                         Toast.makeText(LoginActivity.this, "Flashlet added successfully.", Toast.LENGTH_SHORT).show();
                     }
+                    LoginActivity.this.flashletId = null;
                 }
             } else {
                 Log.e("LoginActivity", "Error checking user document", task.getException());
@@ -295,7 +291,7 @@ public class LoginActivity extends AppCompatActivity {
                                 localDB.addUser(new UserWithRecents(user));
                                 if (flashletId != null) {
                                     handleFlashletAddition(flashletId, currentUser.getUid());
-                                    finish();
+                                    flashletId = null;
                                 }
                                 else {
                                     // Send User to Home Screen
